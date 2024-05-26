92783004d819614de3b5d69e84596deb51151c84
pombadev <11157420+pombadev@users.noreply.github.com>
2024-05-26 23:51:36 +0545

    feat: read input from stdin

--- cli.ml
+++ cli.ml
@@ -1,12 +1,25 @@
 module Semver = Semantic_version.Make ()
 
-type part = [ `Major | `Minor | `Patch ]
-type t = { version : Semver.t; part : part }
+type part = [`Major | `Minor | `Patch]
+
+type t = { version: Semver.t; part: part }
 
 let usage = "semv [-major] [-minor] [-patch] +/- <input>"
+
 let input = ref None
+
 let part = ref None
+
 let preserve = ref false
+
+let read_input () =
+  let buffer = Buffer.create 1024 in
+  try
+    while true do
+      Buffer.add_channel buffer stdin 1
+    done;
+    Buffer.contents buffer
+  with End_of_file -> Buffer.contents buffer
 
 let die msg =
   prerr_endline ("semv: " ^ msg);
@@ -26,19 +39,21 @@ let parse () =
       ]
       (fun ver ->
         match !input with
-        | Some _ -> die "<input> cannot be multiple"
-        | None -> (
-            try
-              let ver = Semver.of_string ver in
-              input := Some ver
-            with _ -> die "<input> is not a valid semver"))
+        | Some _ ->
+            die "<input> cannot be multiple"
+        | None ->
+            input := Some ver )
       usage
   in
-
   let version =
-    match !input with Some ver -> ver | None -> die "<input> is required"
+    let is_pipe = Unix.isatty Unix.stdin |> not in
+    let version =
+      if is_pipe then read_input ()
+      else
+        match !input with Some ver -> ver | None -> die "<input> is required"
+    in
+    try Semver.of_string version with _ -> die "<input> is not a valid semver"
   in
-
   let part =
     match !part with Some part -> part | None -> die "<part> is required"
   in
--- cli.mli
+++ cli.mli
@@ -1,7 +1,9 @@
 module Semver : Semantic_version.S
 
-type part = [ `Major | `Minor | `Patch ]
-type t = { version : Semver.t; part : part }
+type part = [`Major | `Minor | `Patch]
+
+type t = { version: Semver.t; part: part }
 
 val parse : unit -> t
+
 val preserve : bool ref
